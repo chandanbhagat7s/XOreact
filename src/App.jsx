60341fed25189746c1a8d65bbe9ac14c648b0209
chandanbhagat7s <bhagatchandan287@gmail.com>
2023-08-18 17:27:02 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [winner, setWinner] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>XO-GAME</h1>
+        {!winner ? (
+          <div className="b">
+            <Box setWinner={setWinner}></Box>
+          </div>
+        ) : (
+          <div className="btn" onClick={() => setWinner("")}>
+            PLAY AGAIN
+          </div>
+        )}
+        <div className="result">
+          <p>the winner is {winner}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function Box({ setWinner }) {
+  const [pointer, setPointer] = useState("X");
+  const [data, setData] = useState([...Array(9).fill("")]);
+
+  function setContent(i) {
+    pointer == "X" ? setPointer("O") : setPointer("X");
+    let newData = [...data];
+    newData[i] = pointer;
+    setData([...newData]);
+  }
+
+  (function calculate() {
+    // console.log(data);
+    if (
+      (data[0] == data[1] && data[1] == data[2] && data[2] == "X") ||
+      (data[3] == data[4] && data[4] == data[5] && data[5] == "X") ||
+      (data[6] == data[7] && data[7] == data[8] && data[8] == "X") ||
+      (data[0] == data[3] && data[3] == data[6] && data[6] == "X") ||
+      (data[1] == data[4] && data[4] == data[7] && data[7] == "X") ||
+      (data[2] == data[5] && data[5] == data[8] && data[8] == "X") ||
+      (data[0] == data[4] && data[4] == data[8] && data[8] == "X") ||
+      (data[2] == data[4] && data[4] == data[6] && data[6] == "X")
+    ) {
+      setTimeout(() => {
+        setWinner("PLAYER X");
+      }, 10);
+    } else if (
+      (data[0] == data[1] && data[1] == data[2] && data[2] == "O") ||
+      (data[3] == data[4] && data[4] == data[5] && data[5] == "O") ||
+      (data[6] == data[7] && data[7] == data[8] && data[8] == "O") ||
+      (data[0] == data[3] && data[3] == data[6] && data[6] == "O") ||
+      (data[1] == data[4] && data[4] == data[7] && data[7] == "O") ||
+      (data[2] == data[5] && data[5] == data[8] && data[8] == "O") ||
+      (data[0] == data[4] && data[4] == data[8] && data[8] == "O") ||
+      (data[2] == data[4] && data[4] == data[6] && data[6] == "O")
+    ) {
+      setTimeout(() => {
+        setWinner("PLAYER O");
+      }, 10);
+    }
+  })();
+  return (
+    <>
+      <div className="chance">chance of {pointer}</div>
+      <div className="box-container">
+        {" "}
+        {[...Array(9)].map((e, i) => {
+          return (
+            <SmallBox
+              key={i}
+              box={i}
+              data={data[i]}
+              changeSymbol={setContent}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+function SmallBox({ data, box, changeSymbol }) {
+  return (
+    <>
+      <div className="box" onClick={() => changeSymbol(box)}>
+        {data}
+      </div>
+    </>
+  );
+}
+export default App;
